1d4446a07517a3a487fa04666fdc2f336674c621
Alex Kuzko <okuzk@softserveinc.com>
2020-12-04 18:33:49 +0200

    BinaryTreeDemo

---
+++ src/main/java/collections/binarytree/Node.java
@@ -0,0 +1,17 @@
+package collections.binarytree;
+
+public class Node {
+
+    public int key; //ключ узла
+    public String data; //некоторые данные в узле
+    public Node leftChild; //левый потомок
+    public Node rightChild; //правый потомок
+
+    /**
+     * Метод который выводит на экран содержимое узла
+     */
+    public void printNode(){
+        System.out.println("KEY " + key + " DATA: " + data);
+    }
+
+}
